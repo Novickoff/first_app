f3b91f5e62ca7087b237f6b2964107e177270e63
Sergey S. Novickoff <sergey.novickoff@gmail.com>
2012-03-23 15:52:29 +0400

    Inital Baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7b7a8257f5126deab18b7cf1eb9aded6cffc9795558dc5efa95282e96a9606d05a7f381df3cd5e8fb59590f5781391109a4cb241943ffaccfb98d0d380bfb2d0'
